1c2784409864b419610ad5974146a7a8ac44dab4
Mohamed Hani <Mohamed_Hani_1179@fci.helwan.edu.eg>
2024-08-19 00:50:21 +0300

    Add project files.

---
+++ Index/Employee.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Index
+{
+    public class Employee
+    {
+        public static double TAX = 0.03;
+        public string Fname;
+        public string Lname;
+        public int Wage;
+        public int LoggedHours;
+
+        private double Calculate() => Wage * LoggedHours;
+        private double CalculateTax() => Calculate()* TAX;
+        private double CalculateNet() => Calculate()- CalculateTax();
+
+
+        public void Print()
+        {
+            Console.WriteLine();
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine();
+            Console.WriteLine($"Fname :{Fname}");
+            Console.WriteLine($"Lname :{Lname}");
+            Console.WriteLine($"Logged Hours :{LoggedHours}");
+            Console.WriteLine($"Wage :{Wage}");
+            Console.WriteLine($"Salary :{Calculate()}");
+            Console.WriteLine($"deductable Tax : {TAX * 100}% , Tax Amount :{CalculateTax()}$");
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine($"Net Salary :{CalculateNet()}$");
+
+        }
+
+
+    }
+}
---
+++ Index/Program.cs
@@ -0,0 +1,327 @@
+﻿namespace Index
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region important of oop
+            //Employee employee = new Employee();
+            //Console.Write("TAX :");
+            //bool t = double.TryParse(Console.ReadLine(), out double result);
+            //Employee.TAX = result;
+
+            //Console.Write("Fname :");
+            //employee.Fname = Console.ReadLine();
+            //Console.Write("Lname :");
+            //employee.Lname = Console.ReadLine();
+            //Console.Write("Logged Hours :");
+            //employee.LoggedHours = int.Parse(Console.ReadLine());
+            //Console.Write("Wage :");
+            //employee.Wage = int.Parse(Console.ReadLine());
+
+            //employee.Print();
+
+            #endregion
+
+            #region methods
+
+            //// passing by value
+            //// value type 
+            //DoAction d = new DoAction();
+            //var age = 18;
+            //d.DoValuev(age);
+            //// reference type 
+            //int[] arr = { 1, 2, 3 };
+            //d.DoValuer(arr);
+            //Console.WriteLine(arr[2]);
+
+            //// passing by refernce
+            //// value type
+            //var x = 10;
+            //var y = 20;
+            //d.DoRefencev(ref x, ref y);
+            //Console.WriteLine(x);
+            //Console.WriteLine(y);
+
+            //// refernce type
+            //int[] arr1 = { 1, 2, 3 };
+            //d.DoRefencer(ref arr1);
+            ////Console.WriteLine(arr1[2]);
+
+            //// out modifier
+            //int z = 10;
+            //int outer;
+            //int inter = 10100000;
+            //int result = d.outmodifier(z, out outer, in inter);
+            //Console.WriteLine($"{result},{outer}");
+
+            //// params modifier
+            //string s = null;
+            //d.paramms(s, 1, 2, 3, 4, 5, 6, 7);
+
+            //// optional 
+            //d.testOptional();
+
+            //// named argument 
+            //d.NamedArg(y: 20, x: 10, z: 30);
+
+            ////Ref Locals
+            //int[] arrref = { 1, 2, 3, 4 };
+            //arrref[0] = 10;
+            //ref int reef = ref arrref[0];
+            //Console.WriteLine(reef);
+            //Console.WriteLine(arrref[0]);
+
+            #endregion
+
+            #region test
+            //int age ;
+            //test2 t = new test2();
+            //t.me(out age );
+
+
+            #endregion
+
+            #region Problem Solving
+            //int d;
+            //d = Convert.ToInt32(!(30 < 20));
+            //Console.WriteLine(d);
+            //Console.WriteLine(13 / 2 + " " + 13 % 2);
+
+            //int num = 1, z = 5;
+
+            //if (!(num <= 0))
+            //    Console.WriteLine(++num + z++ + " " + ++z);
+            //else
+            //    Console.WriteLine(--num + z-- + " " + --z);
+
+            //string str1 = Console.ReadLine();
+            //string str2 = Console.ReadLine();
+            //str1= str2;
+            //str1 = "HANN";
+            //string str3 = str1 + " " + str2;
+            //Console.WriteLine(str3 );
+
+            //int[] arr1 = { 1, 5, 3, 4, 5 };
+            //int[] arr2 = { 1, 2, 3 };
+            //arr1 = arr2;
+            //arr1[2] = 60;
+            //Console.WriteLine(arr1[2]);
+            //Console.WriteLine(arr1[1]);
+
+            // int x = 0;
+            // int y = 50;
+            // x = y;
+            // Console.WriteLine(x);
+            // Console.WriteLine(y);
+
+            // string s = "hello world";
+            //string ns= s.Substring(3);
+            // Console.WriteLine(ns);
+
+            #endregion
+
+            #region Constructor 
+
+            //Date d = new Date(10, 2000);
+            //Console.WriteLine(d.Print());
+
+            //Ob oo = Ob.Create(1,"Mo","Ha");
+
+
+            //Console.WriteLine(oo.Display());
+
+            #endregion
+
+            #region Properties 
+            Dollar d = new Dollar(1.025M);
+            Console.WriteLine(d.Amount);
+            d.Amount = 1.9990m;
+            Console.WriteLine(d.Amount);
+            #endregion
+
+
+        }
+
+    }
+
+
+    #region Constructor
+    public class Date
+    {
+
+        private static readonly int[] DayToMonth366 = { 0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+        private static readonly int[] DayToMonth365 = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+        private readonly int day;
+        private readonly int month;
+        private readonly int year;
+
+        public Date()
+        {
+
+        }
+
+        public Date(int day, int month, int year)
+        {
+            var Leap = year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
+            if (year > 0 && year <= 9999 && month > 0 && month < 31)
+            {
+                int[] days = Leap ? DayToMonth366 : DayToMonth365;
+                if (day > 0 && day <= days[month])
+                {
+                    this.day = day;
+                    this.month = month;
+                    this.year = year;
+                }
+                else
+                {
+                    this.day = 0;
+                    this.month = 0;
+                    this.year = 0;
+                }
+            }
+            else
+            {
+                this.day = 0;
+                this.month = 0;
+                this.year = 0;
+            }
+        }
+
+        // constructor overloading 
+        public Date(int year) : this(01, 01, year)
+        {
+        }
+        public Date(int month, int year) : this(01, month, year)
+        {
+        }
+
+
+        public string Print()
+        {
+            return $"{day.ToString().PadLeft(2, '0')}/{month.ToString().PadLeft(2, '0')}/{year.ToString().PadLeft(4, '0')}";
+        }
+    }
+    #endregion
+
+
+    #region Object Initializer 
+    public class Ob
+    {
+        public int Id;
+        public string FName;
+        public string LName;
+
+        private Ob(int id,string fname, string lname)
+        {
+            Id = id ;
+            FName = fname ;
+            LName = lname ;
+        }
+
+        public static Ob Create(int id , string fName, string LName)
+        {
+            return new Ob(id,fName, LName) ;
+        }
+        public string Display() => $"Id: {Id} ,FirstName: {FName} ,LastName: {LName}.";
+    }
+    #endregion
+
+    #region Properties
+    public class Dollar
+    {
+        private decimal _amount;
+
+        public decimal Amount
+        {
+            get
+            {
+                return _amount;
+            }
+            set
+            {
+                _amount = ProcessValue(value);
+            }
+        }
+
+        public Dollar(decimal amount)
+        {
+            _amount = ProcessValue(amount);
+        }
+
+        private decimal ProcessValue(decimal value) => value <= 0 ? 0 : Math.Round(value,2);
+    }
+
+    
+    #endregion
+
+
+    //class test2
+    //{
+    //    public int me(out int age)
+    //    {
+    //        age = 10;
+    //        return age;
+    //    }
+    //}
+    //public class DoAction
+    //{
+    //    public int DoValuev(int age)
+    //    {
+    //        age = age * 2;
+    //        return age;
+    //    }
+    //    public void DoValuer(int[] a)
+    //    {
+    //        a[2] = 5;
+    //        a = null;
+    //    }
+    //    public void DoRefencev(ref int x, ref int y)
+    //    {
+    //        var temp = x;
+    //        x = y;
+    //        y = temp;
+    //    }
+
+    //    public void DoRefencer(ref int[] a)
+    //    {
+    //        a[2] = 5;
+    //        a = null;
+    //    }
+
+    //    public int outmodifier(int x, out int y,in int d)
+    //    {
+    //        y = x*d;
+    //        return x * x * y;
+    //    }
+
+
+    //    public void paramms(string s,params int[] a )
+    //    {
+    //        a[0] = 5;
+    //        a[1] = 5;
+    //        Console.WriteLine(a[4]);
+    //        Console.WriteLine(a[2]);
+    //        s= "hap";
+    //        Console.WriteLine(s);
+
+    //    }
+
+    //    public void testOptional(int x =  0, int y = 0)
+    //    {
+    //        Console.WriteLine(x);
+    //        Console.WriteLine(y);
+    //    }
+
+    //    public void NamedArg(int x , int y, int z )
+    //    {
+    //        Console.WriteLine(x);
+    //        Console.WriteLine(y);
+    //        Console.WriteLine(z);
+    //    }
+
+    //}
+
+}
